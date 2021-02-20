54d16f355138e86d25df7ceb476a04defeeabe49
Your Name <you@example.com>
2021-02-20 17:34:09 -0300

    protecting the establishment's routes and changing messages

--- app/controllers/establishment_controller.py
+++ app/controllers/establishment_controller.py
@@ -3,11 +3,13 @@ from app.models.establishment_model import Establishment
 from app.services import establishment_service
 from app.schemas.establishment_schema import EstablishmentSchema
 from app import utils
+from app.security import security_token
 
 establishment_controller = Blueprint('establishment',__name__)
 schema = EstablishmentSchema()
 
 @establishment_controller.route('',methods=['GET'])
+@security_token
 def get_establishments():
     establishments, error = establishment_service.get_establishments()
 
@@ -18,6 +20,7 @@ def get_establishments():
 
 
 @establishment_controller.route('/<int:id>',methods=['GET'])
+@security_token
 def get_by_id_establishment(id):
     establishment,error = establishment_service.get_establishment(id)
 
@@ -25,7 +28,7 @@ def get_by_id_establishment(id):
         return utils.response_bad_request(error)
 
     if establishment is None:
-        return utils.response_not_found('Establishment not found.')
+        return utils.response_not_found('Estabelecimento não encontrado.')
 
     return utils.response_ok(schema.dump(establishment))
 
@@ -33,6 +36,7 @@ def get_by_id_establishment(id):
 
 
 @establishment_controller.route('',methods=['POST'])
+@security_token
 def post_establishment():
     data = request.get_json()
     errors = schema.validate(data)
@@ -51,13 +55,14 @@ def post_establishment():
 
 
 @establishment_controller.route('/<int:id>',methods=['PUT'])
+@security_token
 def put_establishment(id):
     establishment,error = establishment_service.get_establishment(id)
 
     if error:
         return utils.response_bad_request(error)
     elif establishment is None:
-        return utils.response_not_found('Establishment not found.')
+        return utils.response_not_found('Estabelecimento não encontrado.')
 
     data = request.get_json()
     errors = schema.validate(data)
@@ -75,17 +80,18 @@ def put_establishment(id):
 
 
 @establishment_controller.route('/<int:id>',methods=['DELETE'])
+@security_token
 def delete_establishment(id):
     establishment, error = establishment_service.get_establishment(id)
 
     if error:
         return utils.response_bad_request(error)
     elif establishment is None:
-        return utils.response_not_found('Establishment not found.')
+        return utils.response_not_found('Estabelecimento não encontrado.')
 
     success,error = establishment_service.delete_establishment(establishment)
 
     if error:
         return utils.response_bad_request(error)
 
-    return utils.response_ok('establishment successfully deleted.')
+    return utils.response_ok('Estabelecimento excluído com sucesso.')
--- app/security/__init__.py
+++ app/security/__init__.py
@@ -20,21 +20,20 @@ def check_password(password,hashed):
         return False
 
 def generate_token(data):
-    print(get_app().config.get('TIME_EXPIRATION'))
     data['expiration'] = int(time()*1000) + get_app().config.get('TIME_EXPIRATION')
     return enconde(data)
 
 def enconde(data):
-    encoded = jwt.encode(data, get_app().config.get('SECRET_KEY'), algorithm='HS256')
-    return encoded
+    encoded_token = jwt.encode(data, get_app().config.get('SECRET_KEY'), algorithm='HS256')
+    return encoded_token
 
-def decode(enconde):
-    decode = jwt.decode(enconde, get_app().config.get('SECRET_KEY'), algorithm='HS256')
-    return decode
+def decode(enconde_token):
+    decode_token = jwt.decode(enconde_token, get_app().config.get('SECRET_KEY'), algorithms=['HS256'])
+    return decode_token
 
 def validate_token(data):
-    usuario = User.query.filter_by(id=data['id']).first()
-    if(data['expiration'] > int(time()*1000) or not usuario):
+    user = User.query.filter_by(id=data['id']).first()
+    if(int(time()*1000) > data['expiration'] or not user):
         return False
     return True
 
@@ -43,14 +42,16 @@ def security_token(func):
     def wrapper(*args, **kwargs):
         from flask import request
         token = request.headers.get('Authorization')
+
         error = {'error':'invalid token'},401
-        if not token:
+        if not token or not token.startswith('Bearer '):
             return error
+
         try:
-            token_decoded = decode(token)
+            token_decoded = decode(token.replace('Bearer ',''))
             if not validate_token(token_decoded):
                 return error
-        except:
+        except Exception as e:
             return error
 
         return func(*args, **kwargs)
